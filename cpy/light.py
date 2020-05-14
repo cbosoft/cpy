fd6ec8be848464c8dfe77a74dd63a521b8bd9ba2
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-14 12:23:15 +0100

    Updated LED

--- cpy/light.py
+++ cpy/light.py
@@ -1,23 +1,12 @@
 import numpy as np
 
-from cpy.util import trf
+from cpy.util import trf, arrow
 from cpy.rectifier import Diode
 
 class LED(Diode):
 
-    def __init__(self, x, y, rotation=0):
-        super().__init__(x, y, rotation)
-
     def data(self):
-        pts = [
-                (-1,1), (-1,-1),
-                (np.nan, np.nan),
-                (-1,0), (1,1),
-                (1,-1), (-1,0),
-                (np.nan, np.nan),
-                (1, 0),
-                (np.nan, np.nan),
-            ]
+        pts = super().data()
 
         apts = [
                 (-0.3,0.8), (-0.3,1.3),
@@ -28,5 +17,8 @@ class LED(Diode):
                 (np.nan, np.nan),
                 (0.5, 1.1), (0.3, 1.3), (0.1, 1.1),
             ]
-        pts.extend(apts)
+
+        #pts.extend(apts)
+        pts.extend(arrow((0.3,1.2), length=0.8, rotation=(26.7, -0.3, 0.0) ))
+        pts.extend(arrow((-0.3,1.2), length=0.8, rotation=(26.7, -0.9, 0.0) ))
         return pts

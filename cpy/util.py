70b9e23fccd649935df96c69e70f0ed7acfda045
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-19 10:16:54 +0100

    Removed unused funcs

--- cpy/util.py
+++ cpy/util.py
@@ -76,51 +76,10 @@ def arrow(start, length=None, end=None, rotation=0, inline=False, backwards=Fals
     return list(zip(x, y))
 
 
-def circle(centre, radius):
-    px = np.linspace(0, radius)
-    py = np.sqrt(np.subtract(radius*radius, np.power(px, 2.0)))
-
-    rpx = list(reversed(px))
-    rpy = list(reversed(py))
-
-    nx = -px
-    ny = -py
-
-    rnx = list(reversed(nx))
-    rny = list(reversed(ny))
-
-    cx = [*px, *rpx, *nx, *rnx]
-    cy = [*py, *rny, *ny, *rpy]
-    x, y = trf(cx, cy, *centre)
-    return list(zip(x, y))
-
-
-def arc(centre, radius, start, end, inline=False):
-    thetas = np.linspace(start, end)
-    
-    arc = list()
-    for theta in thetas:
-        x, y = rot(0, radius, theta)
-        arc.append( [x,y] )
-
-    if not inline:
-        arc.insert(0, null)
-        arc.append(null)
-
-    x, y = zip(*arc)
-    return trf(x, y, *centre)
-
-
-def hop(centre, horizontal=False, backwards=False):
-
-    angles = [0,-180]
     if horizontal:
-        angles = np.add(angles, 90)
 
     if backwards:
-        angles = np.add(angles, 180)
 
-    return list(zip(*arc(centre, 0.3, *angles, inline=True)))
 
 def arrowhead(centre, rotation=0, tail=None):
     x, y = centre

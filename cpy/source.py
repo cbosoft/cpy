d8f004ebec1e6701d9aaaddd91dfba126969d28b
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-20 10:00:34 +0100

    Custom labelling for sources

--- cpy/source.py
+++ cpy/source.py
@@ -1,4 +1,5 @@
 from cpy.node import Node
+from cpy.tikz import pic
 
 class Source(Node):
 
@@ -8,3 +9,11 @@ class Source(Node):
 
     def ports(self):
         return (2.0, 0.0)
+
+    def draw_label(self):
+        x, y = self.trot(0, 0.75)
+        pic().draw_text(x, y, self.label, anchor='center', rotation=self.angle)
+
+    def draw_value(self):
+        x, y = self.trot(0, -0.75)
+        pic().draw_text(x, y, self.value, anchor='center', rotation=self.angle)
